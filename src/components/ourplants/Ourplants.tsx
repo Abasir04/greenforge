2f09d679cfa51c678e8b1bedfbf7d10d90de664c
Abasir <adiadebayo1@gmail.com>
2024-05-21 17:25:07 +0100

    ourplants design done

--- src/components/ourplants/Ourplants.tsx
+++ src/components/ourplants/Ourplants.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import './Ourplants.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMobileButton } from "@fortawesome/free-solid-svg-icons";
+
 
 const plantTypes = [
     'Outdoor Plants',
@@ -9,32 +13,41 @@ const plantTypes = [
     'Indoor Plants'
 ]
 
-const items = Array(6).fill(null);
+const plantImg = [
+    "../../assets/images/snakePlant.png",
+    "../../assets/images/cactusPlant.png",
+    "../../assets/images/zzPlant.png"
+]
 
 const Ourplants = () => {
     return (
         <div className="ourplants">
-            <div className="ourplants-header"></div>
-            <div className="ourplants-types">
-                {plantTypes.map((plantType, index) => (
-                    <div key={index} className="ourplants-types-item">
-                    {plantType}
-                    </div>
-                ))}
+            <div className="ourplants__header">
+                <h2>
+                    Our <span>Plants</span>
+                </h2>
             </div>
-            <div className="ourplants-images">
-                <div className="ourplants-images-menu">
-                    {items.map((_, index) => (
-                        <div key={index} className="ourplants-images-menu-item">
-                        <div className="ourplants-images-menu-item-picture"></div>
-                        <div className="ourplants-images-menu-item-text"></div>
+            <div className="ourplants__content">
+                <div className="ourplants__types">
+                    {plantTypes.map((plantType, index) => (
+                        <div key={index} className="ourplants__type-item">
+                            {plantType}
                         </div>
                     ))}
+                </div>
+                <div className="ourplants__images">
+                    <div className="ourplants__images-menu">
+                        {plantImg.map((imgSrc, index) => (
+                            <div key={index} className="ourplants__images-menu-item">
+                                <img src={imgSrc} alt={`Plant ${index + 1}`} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-                <div className="ourplants-images-nextButtons"></div>
-            </div>
+            <div className="ourplants__images-next-buttons"></div>
         </div>
-    )
+    );
 }
 
 export default Ourplants
